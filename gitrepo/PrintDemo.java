304050bc2cfcb66098d135105b8dfa5cd2ae1049
kiranbanda420 <31721984+kiranbanda420@users.noreply.github.com>
2018-03-08 10:51:06 +0530

    jenkins build demo

---
+++ gitrepo/PrintDemo.java
@@ -0,0 +1,8 @@
+package gitbuildtest;
+
+public class PrintDeom {
+	public static void main(String[] args){
+		//todo Auto-generated method stub
+	System.out.println("this is a jenkind build demo ");
+	}
+}
