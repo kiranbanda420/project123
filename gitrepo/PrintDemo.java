84cc088db683484ed4107d44b310f715e3159ced
kiranbanda420 <31721984+kiranbanda420@users.noreply.github.com>
2018-03-08 11:43:14 +0530

    jenkins build on changes

--- gitrepo/PrintDemo.java
+++ gitrepo/PrintDemo.java
@@ -4,6 +4,6 @@ package gitbuildtest;
 public class PrintDeom {
 	public static void main(String[] args){
 		//todo Auto-generated method stub
-	System.out.println("This is a jenkins build demo sample ");
+	System.out.println("This is a jenkins build demo sample File ");
 	}
 }
